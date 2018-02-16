6ec631f3059842dc2864cb1228ff36fa38ad80bc
aligurbuz <galiant781@gmail.com>
2018-02-16 20:07:45 +0000

    boot app

---
+++ src/resta/Console/Source/Boot/Boot.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Resta\Console\Source\Boot;
+
+use Resta\Console\ConsoleListAccessor;
+use Resta\Console\ConsoleOutputter;
+use Resta\StaticPathModel;
+use Resta\Utils;
+
+class Boot extends ConsoleOutputter {
+
+    use ConsoleListAccessor;
+
+    /**
+     * @var $type
+     */
+    public $type='boot';
+
+    /**
+     * @var $define
+     */
+    public $define='Boot create';
+
+    /**
+     * @var $command_create
+     */
+    public $command_create='php api boot create [boot]';
+
+    /**
+     * @method create
+     * @return mixed
+     */
+    public function create(){
+
+        $this->touch['main/boot']        = StaticPathModel::bootDir().'/'.$this->argument['project'].'.php';
+
+        $this->file->touch($this);
+
+        Utils::chmod(root);
+
+        echo $this->classical('---------------------------------------------------------------------------');
+        echo $this->bluePrint('Booting Class Named ['.$this->argument['project'].'] Has Been Successfully Created');
+        echo $this->classical('---------------------------------------------------------------------------');
+        echo $this->cyan('   You can see in src/boot your boot class   ');
+        echo $this->classical('---------------------------------------------------------------------------');
+    }
+}
