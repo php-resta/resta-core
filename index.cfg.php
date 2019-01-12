c1ee6186f32a45616a7edf2485937a5c89423cd3
aligurbuz <galiant781@gmail.com>
2019-01-12 10:27:04 +0000

    resta unit test core system

--- index.cfg.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: aliemirgurbuz
- * Date: 6.11.2017
- * Time: 22:46
- */
---
+++ tests/Config/ConfigTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Resta\Core\Tests\Config;
+
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    /**
+     * @return void|mixed
+     */
+    public function testBase()
+    {
+        $this->assertTrue(true);
+    }
+}
