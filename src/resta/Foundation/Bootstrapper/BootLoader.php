27b2bc41ef4b09851db75ab54919d35355fd7b72
aligurbuz <galiant781@gmail.com>
2019-01-19 09:51:25 +0000

    bootloader fix

--- src/resta/Foundation/Bootstrapper/BootLoader.php
+++ src/resta/Foundation/Bootstrapper/BootLoader.php
@@ -43,7 +43,7 @@ class BootLoader extends ApplicationProvider implements BootContracts
     {
         //if the console is true
         //console app runner
-        if(Utils::isRequestConsole()){
+        if(Utils::isRequestConsole() and core()->isAvailableStore){
 
             //If the second parameter is sent true to the application builder,
             //all operations are performed by the console and the custom booting are executed
