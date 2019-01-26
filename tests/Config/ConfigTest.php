a45c26d4e7f5dcfce436900c395268c07406d387
aligurbuz <galiant781@gmail.com>
2019-01-26 11:18:48 +0000

    core config test

--- tests/Config/ConfigTest.php
+++ tests/Config/ConfigTest.php
@@ -2,6 +2,7 @@
 
 namespace Resta\Core\Tests\Config;
 
+use Resta\Config\Config;
 use PHPUnit\Framework\TestCase;
 use Resta\Foundation\Application;
 
@@ -10,28 +11,46 @@ class ConfigTest extends TestCase
     /**
      * @var $app Application
      */
-    protected $app;
+    protected static $app;
 
     /**
      * @return void|mixed
      */
     protected function setUp()
     {
-        $this->app = new Application(false);
+        if(static::$app===null){
+            static::$app = new Application(false);
+        }
 
-        $this->app->loadConfig(function()
+        static::$app->loadConfig(function()
         {
-            return ['core'=>['test1'=>'foo','test2'=>'bar']];
+            return ['core'=>[
+                'test1'=>'foo',
+                'test2'=>[
+                    'nested1'=>'nested1value'
+                ]
+                ]
+            ];
         });
-
-        parent::setUp();
     }
 
     /**
      * @return void|mixed
      */
-    public function testBase()
+    public function testConfigHelper()
     {
         $this->assertSame('foo',config('core.test1'));
+        $this->assertSame('nested1value',config('core.test2.nested1'));
     }
+
+    /**
+     * @throws \DI\DependencyException
+     * @throws \DI\NotFoundException
+     */
+    public function testBase()
+    {
+        $this->assertSame('foo',Config::make('core.test1')->get());
+        $this->assertSame('nested1value',Config::make('core.test2.nested1')->get());
+    }
+
 }
