a2814c8ed852e48d30a7d191b7b7b31765671a11
aligurbuz <galiant781@gmail.com>
2019-02-26 09:20:34 +0000

    service provider formatting

--- src/resta/Provider/ServiceProvider.php
+++ src/resta/Provider/ServiceProvider.php
@@ -2,18 +2,53 @@
 
 namespace Resta\Provider;
 
+use Resta\Support\Utils;
 use Resta\Foundation\ApplicationProvider;
 
 class ServiceProvider extends  ApplicationProvider
 {
     /**
+     * handle service providers
+     *
      * @return void|mixed
      */
     public function handle()
     {
-        if(config('kernel')!==null){
-            foreach(config('kernel.providers') as $key=>$provider){
-                $this->app->makeBind($provider)->boot();
+        //get kernel service providers for application
+        $providers = config('kernel.providers');
+
+        if(is_array($providers)){
+
+            //first we are running register methods of provider classes.
+            foreach($providers as $key=>$provider){
+                $this->applyProvider($provider);
+                $this->app->register('providers',$key,$provider);
+            }
+
+            //then we are running boot methods of provider classes.
+            foreach($providers as $key=>$provider){
+                $this->applyProvider($provider,'boot');
+            }
+        }
+    }
+
+    /**
+     * apply provider class
+     *
+     * @param $provider
+     * @param string $method
+     */
+    public function applyProvider($provider,$method='register')
+    {
+        // If the provider classes are a real object
+        // we will run them.
+        if(Utils::isNamespaceExists($provider)){
+
+            // after determining whether the register or boot methods
+            // we are running the provider.
+            $providerInstance = $this->app->makeBind($provider);
+            if(method_exists($providerInstance,$method)){
+                $providerInstance->{$method}();
             }
         }
     }
