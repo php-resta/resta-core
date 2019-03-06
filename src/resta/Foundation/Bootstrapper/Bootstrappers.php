dd2227d9daacbf3433fd7a77c9573952d37b84c1
aligurbuz <galiant781@gmail.com>
2019-03-06 19:55:57 +0000

    bootstrappers formatting

--- src/resta/Foundation/Bootstrapper/Bootstrappers.php
+++ src/resta/Foundation/Bootstrapper/Bootstrappers.php
@@ -10,7 +10,7 @@ class Bootstrappers
     /**
      * @var array $stack
      */
-    protected $stack=[];
+    protected $stack = [];
 
     /**
      * @var $app ApplicationContracts
@@ -18,42 +18,29 @@ class Bootstrappers
     protected $app;
 
     /**
-     * @var $bootstrapper null
-     */
-    protected $bootstrapper;
-
-    /**
      * @var $pusher array
      */
-    protected $pusher=array();
+    protected $pusher = array();
 
     /**
      * @var $pusherStacks array
      */
-    protected $pusherStacks=array();
+    protected $pusherStacks = array();
 
     /**
      * Bootstrappers constructor.
-     * @param null $concrete
+     *
+     * @param null $app
      * @param array $pusher
-     * @param null $bootstrapper
      */
-    public function __construct($app=null,$pusher=array(),$bootstrapper=null)
+    public function __construct($app=null,$pusher=array())
     {
-        //if the user sets the bootstrapper variable to true,
-        //we do not do anything.
-        if($bootstrapper===true){
-            throw new \LogicException('bootstrapper is not available');
-        }
-
-
         //The concrete object is the callback class itself that is sent to this class.
         //Once the concrete object has been assigned, we run the bootstrappers sequence
         //and include it in the application.
         //The pusher and bootstrapper variables can be sent by the user.
         $this->app          = $app;
         $this->pusher       = $pusher;
-        $this->bootstrapper = $bootstrapper;
 
         //we boot the initial instance for the application.
         if($this->ifExistPusher()) (new BaseRegister($this->app))->handle();
@@ -69,20 +56,15 @@ class Bootstrappers
     }
 
     /**
-     * @param bool $bootstrapper
-     * @return mixed
+     * get bootstrappers list
+     *
+     * @return array
      */
-    private function getBootstrappers($bootstrapper=false)
+    private function getBootstrappers()
     {
-        //if a bootstrapper variable is sent as false to the installer object,
-        //the bootstrapper will be assigned as false this variable directly.
-        if(false===$this->bootstrapper){
-            $bootstrapper=$this->bootstrapper;
-        }
-
         //if the value to be sent as the second parameter for this method is true,
         //the values ​​in the first parameter of the method will be combined with the bootstrappers list.
-        if($bootstrapper && $this->ifExistPusher()){
+        if($this->ifExistPusher()){
             $bootstrapStack=array_merge($this->pusher,$this->app->kernelGroupKeys());
         }
 
@@ -117,11 +99,11 @@ class Bootstrappers
     private function peelings()
     {
         //if there are peelings
-        if(isset(core()->peelings)){
+        if(isset($this->app['peelings'])){
 
             // We send the peelings property to
             // the bootstrapperPeelOnion class.
-            $peelings=core()->peelings;
+            $peelings = $this->app['peelings'];
             pos($peelings)->onionRun($peelings);
         }
     }
@@ -136,7 +118,7 @@ class Bootstrappers
         // we are running a boot on condition
         // that it accepts an object and array logic belonging
         // to this class that is run outside the kernel system.
-        $checkPusher=(count($this->pusher)=='0') ? true : false;
+        $checkPusher = (count($this->pusher)=='0') ? true : false;
 
         //pusher is run if there is no callback.
         if(!is_callable($callback)){
@@ -158,16 +140,16 @@ class Bootstrappers
     private function pusherHandle()
     {
         //pusher stack
-        $pusherStack=[];
+        $pusherStack = [];
 
         // we push the collected pusher data to
         // the pusherStacks data with the concrete object.
         foreach ($this->pusher as $pusher){
-            $pusherStack[]=$this->app->{$pusher}();
+            $pusherStack[] = $this->app->{$pusher}();
         }
 
         //pusherHandle key of the pusherStack array
-        $this->pusherStacks['pusherHandle']=$pusherStack;
+        $this->pusherStacks['pusherHandle'] = $pusherStack;
     }
 
     /**
@@ -178,7 +160,7 @@ class Bootstrappers
         // here we check that a special bootstrappers list will work and we identify the onion identifier.
         // we are peeling onion class by classifying onion class.
         $customBootstrapersCount            = count($customBootstrapers);
-        $getBootstrappers                   = $this->getBootstrappers(true);
+        $getBootstrappers                   = $this->getBootstrappers();
         $this->stack['getBootstrappers']    = ($customBootstrapersCount) ? $customBootstrapers : $getBootstrappers;
         $this->stack['onionIdentifier']     = ($customBootstrapersCount) ? false : true;
     }
