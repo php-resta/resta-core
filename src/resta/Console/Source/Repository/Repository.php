b4ac4b8d5f547498f4cf74ca2eb8970ecb923f3c
aligurbuz <galiant781@gmail.com>
2019-03-09 14:01:14 +0000

    respository stub annotations formatting

--- src/resta/Console/Source/Repository/Repository.php
+++ src/resta/Console/Source/Repository/Repository.php
@@ -104,7 +104,7 @@ class Repository extends ConsoleOutputter {
      */
     private function setAnnotations(){
 
-        return Utils::changeClass(path()->kernel().'/AppAnnotations.php',
+        return Utils::changeClass(path()->version().'/ServiceAnnotationsController.php',
             ['Trait ServiceAnnotationsController'=>'Trait ServiceAnnotationsController'.PHP_EOL.' * @method \\'.app()->namespace()->repository().'\\'.$this->argument['repository'].'\\'.$this->argument['repository'].'Contract '.lcfirst($this->argument['repository']).'Repository'
             ]);
     }
