3065280edc5861ffa2ab8c0ec2817e927da95fcc
aligurbuz <galiant781@gmail.com>
2019-04-08 11:20:26 +0000

    event dispatcher formatting

--- src/resta/EventDispatcher/EventHandler.php
+++ src/resta/EventDispatcher/EventHandler.php
@@ -12,24 +12,6 @@ class EventHandler extends EventDetached implements EventDispatcherContracts
     private $events;
 
     /**
-     * @var array
-     */
-    protected $dispatches=[];
-
-    /**
-     * @param array $subscriber
-     * @return mixed|void
-     */
-    public function addSubscriber($subscriber=array())
-    {
-        // To take advantage of an existing event,
-        // you need to connect a listener to the dispatcher
-        // so that it can be notified when the event is dispatched.
-        // A call to the dispatcher's addListener() method associates any valid PHP callable to an event:
-        $this->listen=array_merge($this->getListeners(),[$this->event=>['subscriber'=>$subscriber]]);
-    }
-
-    /**
      * set event for application
      *
      * @param $eventName
@@ -46,6 +28,19 @@ class EventHandler extends EventDetached implements EventDispatcherContracts
             //we save to kernel object value of the event-dispatcher
             $this->app->register('events',$eventName,$this->events[$eventName]);
         }
+    }
+
+    /**
+     * @param array $subscriber
+     * @return mixed|void
+     */
+    public function addSubscriber($subscriber=array())
+    {
+        // To take advantage of an existing event,
+        // you need to connect a listener to the dispatcher
+        // so that it can be notified when the event is dispatched.
+        // A call to the dispatcher's addListener() method associates any valid PHP callable to an event:
+        $this->listen=array_merge($this->getListeners(),[$this->event=>['subscriber'=>$subscriber]]);
     }
 }
 
