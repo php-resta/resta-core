6bafb1fb8e88b60b773e3060d03dc9c0efc4bc6b
aligurbuz <galiant781@gmail.com>
2019-04-13 14:14:34 +0000

    application base register test

---
+++ tests/Foundation/ApplicationBaseRegisterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Resta\Core\Tests\Foundation;
+
+use Resta\Foundation\ApplicationBaseRegister;
+use Resta\Support\Dependencies;
+use Resta\Core\Tests\AbstractTest;
+
+class ApplicationBaseRegisterTest extends AbstractTest
+{
+    /**
+     * @var array
+     */
+    protected $values = [];
+
+    /**
+     * @return void|mixed
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @return void|mixed
+     */
+    public function testApplicationBaseRegister()
+    {
+        $this->assertTrue(true,defined('appInstance'));
+        $this->assertTrue(true,isset($this->app['revision']));
+        $this->assertTrue(true,isset($this->app['corePath']));
+        $this->assertTrue(true,isset($this->app['container']));
+        $this->assertTrue(true,isset($this->app['appClass']));
+        $this->assertTrue(true,isset($this->app['appClosureInstance']));
+        $this->assertTrue(true,isset($this->app['closureBootLoader']));
+        $this->assertTrue(true,isset($this->app['macro']));
+        $this->assertTrue(true,isset($this->app['instanceController']));
+        $this->assertTrue(true,isset($this->app['responseSuccess']));
+        $this->assertTrue(true,isset($this->app['responseStatus']));
+        $this->assertTrue(true,isset($this->app['responseType']));
+        $this->assertTrue(true,isset($this->app['out']));
+        $this->assertTrue(true,isset($this->app['request']));
+        $this->assertTrue(true,isset($this->app['get']));
+        $this->assertTrue(true,isset($this->app['post']));
+        $this->assertTrue(true,isset($this->app['httpMethod']));
+        $this->assertTrue(true,isset($this->app['fileSystem']));
+        $this->assertTrue(true,defined('httpMethod'));
+
+    }
+
+}
