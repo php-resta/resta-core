ae7cfb5f9ef5c1239df299981e83c7d68b66deff
aligurbuz <galiant781@gmail.com>
2019-05-09 19:33:31 +0000

    url parse exception fix

--- src/resta/Url/UrlParseException.php
+++ src/resta/Url/UrlParseException.php
@@ -2,33 +2,37 @@
 
 namespace Resta\Url;
 
+use DomainException;
 use Resta\Foundation\PathManager\StaticPathModel;
 
 class UrlParseException
 {
     /**
-     * @param null $data
+     * @param array $data
      */
-    public function exception($data=null)
+    public function exception($data=array())
     {
+        if(!isset($data['project']) and !isset($data['version'])){
+            throw new DomainException('No Project or Version');
+        }
+
         //get app path for checking
-        $appPath=StaticPathModel::appPath().'/'.$data['project'];
+        $appPath = StaticPathModel::appPath().'/'.$data['project'];
 
         //If there is no project on the url
         //we throw an exception
         if($data['project']===null OR !file_exists($appPath)){
-            throw new \DomainException('No Project');
+            throw new DomainException('No Project');
         }
-        
 
         if(!in_array($data['version'],UrlVersionIdentifier::supportedVersions())){
-            throw new \DomainException('Version Number is not supported');
+            throw new DomainException('Version Number is not supported');
         }
 
         //If there is no endpoint on the url
         //we throw an exception
         if($data['endpoint']===null){
-            throw new \DomainException('No Endpoint');
+            throw new DomainException('No Endpoint');
         }
     }
 }
