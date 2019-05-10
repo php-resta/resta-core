fcec12f3ed6280356f6f42ee5f7b7867bff67013
aligurbuz <galiant781@gmail.com>
2019-05-10 14:48:17 +0000

    bootstrappers fix

--- src/resta/Foundation/Bootstrapper/Bootstrappers.php
+++ src/resta/Foundation/Bootstrapper/Bootstrappers.php
@@ -70,7 +70,7 @@ class Bootstrappers
     /**
      * kernel boot manager method
      *
-     * @param string $boot
+     * @param array|null $boot
      * @param string $maker
      * @return mixed
      */
@@ -78,7 +78,7 @@ class Bootstrappers
     {
         // we can refer to this method
         // because we can boot classes in the middleware or bootstrapper array.
-        if(!is_array($boot) && !is_null($maker)){
+        if(is_null($boot) && !is_null($maker)){
 
             /** @var KernelManifestManager $kernelManifestBind */
             $kernelManifestBind = $this->app->resolve(KernelManifestManager::class);
