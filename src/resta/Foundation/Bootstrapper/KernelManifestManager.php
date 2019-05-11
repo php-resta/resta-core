9c25a44a8dac630dfbb4f233036c6863b4a42c92
aligurbuz <galiant781@gmail.com>
2019-05-11 10:20:09 +0000

    kernel manifest manager fix

--- src/resta/Foundation/Bootstrapper/KernelManifestManager.php
+++ src/resta/Foundation/Bootstrapper/KernelManifestManager.php
@@ -9,7 +9,7 @@ use Resta\Contracts\ApplicationContracts;
 class KernelManifestManager
 {
     /**
-     * @var $app ApplicationContracts
+     * @var ApplicationContracts $app
      */
     protected $app;
 
@@ -19,7 +19,7 @@ class KernelManifestManager
     protected $makerList=[];
 
     /**
-     * @var $manifest
+     * @var object $manifest
      */
     protected $manifest;
 
