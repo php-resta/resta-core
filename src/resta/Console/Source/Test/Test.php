874f16ac1a950544ef383eaaffb115a99ae500a9
aligurbuz <galiant781@gmail.com>
2019-05-21 13:20:33 +0000

    test console run

--- src/resta/Console/Source/Test/Test.php
+++ src/resta/Console/Source/Test/Test.php
@@ -2,8 +2,12 @@
 
 namespace Resta\Console\Source\Test;
 
+use Resta\Support\PhpUnitManager;
+use Resta\Support\SimpleXmlManager;
 use Resta\Console\ConsoleOutputter;
 use Resta\Console\ConsoleListAccessor;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class Test extends ConsoleOutputter {
 
@@ -22,7 +26,7 @@ class Test extends ConsoleOutputter {
     /**
      * @var $commandRule
      */
-    public $commandRule = ['test'];
+    public $commandRule = ['create'=>['test']];
 
     /**
      * @method generate
@@ -46,5 +50,42 @@ class Test extends ConsoleOutputter {
         $this->file->touch($this);
 
         echo $this->classical(' > Test file called as "'.$this->argument['test'].'" has been successfully created in the '.app()->namespace()->tests().'');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function publish()
+    {
+        $phpunit = ''.root.'/phpunit.xml';
+
+        $xml = new SimpleXmlManager($phpunit);
+
+        $array = $xml->toArray();
+
+        $new = (new PhpUnitManager($array))->add(strtolower($this->projectName()),'directory',
+            str_replace(root.''.DIRECTORY_SEPARATOR,"",app()->path()->tests()));
+
+        $newDataXml = $xml->toXml($new);
+
+        app()->get('fileSystem')->writeFile($phpunit,$newDataXml);
+
+        echo $this->classical(' > phpunit.xml file has been updated');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function run()
+    {
+        $process = new Process(['vendor'.DIRECTORY_SEPARATOR.'bin'.DIRECTORY_SEPARATOR.'phpunit','--group',strtolower($this->projectName())]);
+
+        try {
+            $process->mustRun();
+
+            echo $process->getOutput();
+        } catch (ProcessFailedException $exception) {
+            echo $exception->getMessage();
+        }
     }
 }
