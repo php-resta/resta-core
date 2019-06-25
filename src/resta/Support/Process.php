21f1bb707f043a24574a7c836f84f4b0cd98de43
alimunch <ali@munch.com.tr>
2019-06-25 12:48:14 +0000

    support process

---
+++ src/resta/Support/Process.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Resta\Support;
+
+use Symfony\Component\Process\Process as ProcessHandler;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class Process
+{
+    /**
+     * command process
+     *
+     * @param null|string $command
+     */
+    public function command($command=null)
+    {
+        $process = new ProcessHandler($command,root.'');
+        $process->run();
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        echo $process->getOutput();
+    }
+}
