a6c33a3cec9a5bd8400e51af6bbb68e41b84b0f3
aligurbuz <galiant781@gmail.com>
2019-07-04 15:14:07 +0000

    support general class fix

--- src/resta/Support/Generator.php
+++ src/resta/Support/Generator.php
@@ -2,10 +2,21 @@
 
 namespace Resta\Support;
 
+use Nette\PhpGenerator\PhpNamespace;
 use Resta\Exception\FileNotFoundException;
 
 class Generator
 {
+    /**
+     * @var null|array
+     */
+    protected $classProperties;
+
+    /**
+     * @var null|string
+     */
+    protected $data;
+
     /**
      * @var null|string
      */
@@ -42,19 +53,205 @@ class Generator
     protected $stubPath;
 
     /**
-     * Generator constructor.
-     * @param null $path
-     * @param null $fileSystem
+     * @var array
      */
-    public function __construct($path=null,$fileSystem=null)
+    protected $accessibleProperties = array();
+
+    /**
+     * @var array
+     */
+    protected $methodParameters = array();
+
+    /**
+     * Generator constructor.
+     * @param null|string $path
+     * @param null|string $name
+     * @param null|object $fileSystem
+     */
+    public function __construct($path=null,$name=null,$fileSystem=null)
     {
         $this->path = $path;
 
-        $this->fileSystem = $fileSystem;
+        $this->name = $name;
+
+        $this->file = $this->path.''.DIRECTORY_SEPARATOR.''.ucfirst($this->name).'.php';
+
+        $this->fileSystem = (is_null($fileSystem)) ? files() : $fileSystem;
 
         $this->namespace = Utils::getNamespace($this->path);
 
+        $this->setStubPath();
+
         $this->createPath();
+    }
+
+    /**
+     * creates class for generator
+     *
+     * @param array $replacement
+     * @return Generator
+     *
+     * @throws FileNotFoundException
+     */
+    public function createClass($replacement=array())
+    {
+        if(file_exists($this->path)){
+
+            $content = $this->fileSystem->get($this->getStubFile());
+
+            if($this->fileSystem->put($this->file,$content)!==FALSE){
+                $this->replaceFileContent($replacement,$this->file);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * creates class property for generator
+     *
+     * @param array $properties
+     * @param bool $loadedMethod
+     *
+     * @throws FileNotFoundException
+     */
+    public function createClassProperty($properties=array(),$loadedMethod=false)
+    {
+        if(is_null($this->classProperties)){
+            $this->classProperties = $properties;
+        }
+
+        if($loadedMethod){
+
+            foreach ($this->classProperties as $property) {
+
+                if(preg_match('@class\s.*\n{@',$this->fileSystem->get($this->file),$parse)){
+                    $this->replaceFileContent([
+                        $parse[0] => $parse[0].' 
+    '.$property.'
+    '
+
+                    ],$this->file,true);
+                }
+
+
+            }
+        }
+    }
+
+    /**
+     * creates method for generator
+     *
+     * @param array $methods
+     *
+     * @throws FileNotFoundException
+     */
+    public function createMethod($methods=array())
+    {
+        $list = [];
+
+        foreach ($methods as $method){
+
+            $list[] = '
+    '.$this->getAccessibleMethodValue($method).' function '.$method.'('.$this->getMethodParameters($method).')
+    {
+        return true;
+    }
+            ';
+        }
+        if(preg_match('@class\s.*\n{@',$this->fileSystem->get($this->file),$parse)){
+            $this->replaceFileContent([
+                $parse[0] => $parse[0].' '.implode('',$list)
+            ],$this->file,true);
+
+            $this->createClassProperty([],true);
+        }
+
+    }
+
+    /**
+     * accessible properties method for generator
+     *
+     * @param array $methods
+     * @return void|mixed
+     */
+    public function createMethodAccessibleProperty($methods=array())
+    {
+        foreach($methods as $method=>$accessibleValue)
+        {
+            $this->accessibleProperties[$method] = $accessibleValue;
+
+            $this->replaceFileContent([
+
+                'public function '.$method.'' => ''.$this->getAccessibleMethodValue($method).' function '.$method.''
+
+            ],$this->file,true);
+        }
+    }
+
+    /**
+     * creates method for generator
+     *
+     * @param array $methods
+     */
+    public function createMethodBody($methods=array())
+    {
+        foreach ($methods as $method=>$body){
+
+            $this->replaceFileContent([
+                ''.$this->getAccessibleMethodValue($method).' function '.$method.'\('.$this->getMethodParameters($method).'\)\n.*{\n.*\n.*}' => ''.$this->getAccessibleMethodValue($method).' function '.$method.'('.$this->getMethodParameters($method).')
+    {
+        '.$body.'
+    }'
+            ],$this->file,true);
+        }
+    }
+
+    /**
+     * creates method for generator
+     *
+     * @param array $methods
+     */
+    public function createMethodDocument($methods=array())
+    {
+        foreach ($methods as $method=>$documents){
+
+            $documentString = [];
+            $documentString[] = '/**';
+
+            foreach ($documents as $document){
+                $documentString[] = '
+     * '.$document.'';
+            }
+
+            $documentString[] = '
+     */';
+
+            $this->replaceFileContent([
+                ''.$this->getAccessibleMethodValue($method).' function '.$method.'\('.$this->getMethodParameters($method).'\)' => ''.implode('',$documentString).'
+    '.$this->getAccessibleMethodValue($method).' function '.$method.'('.$this->getMethodParameters($method).')'
+            ],$this->file,true);
+        }
+    }
+
+    /**
+     * accessible properties method for generator
+     *
+     * @param array $methods
+     * @return void|mixed
+     */
+    public function createMethodParameters($methods=array())
+    {
+        foreach($methods as $method=>$parameter)
+        {
+            $this->methodParameters[$method] = $parameter;
+
+            $this->replaceFileContent([
+
+                ''.$this->getAccessibleMethodValue($method).' function '.$method.'\(\)' => ''.$this->getAccessibleMethodValue($method).' function '.$method.'('.$parameter.')'
+
+            ],$this->file,true);
+        }
     }
 
     /**
@@ -72,39 +269,29 @@ class Generator
     }
 
     /**
-     * creates file for generator
+     * get accessible method value for generator
      *
-     * @param null $name
-     * @param array $replacement
-     *
-     * @return Generator
-     *
-     * @throws FileNotFoundException
+     * @param $method
+     * @return mixed|string
      */
-    public function createFile($name=null,$replacement=array())
+    private function getAccessibleMethodValue($method)
     {
-        if(file_exists($this->path) && !is_null($name)){
-
-            $content = $this->fileSystem->get($this->getStubFile());
-            $this->file = $this->path.''.DIRECTORY_SEPARATOR.''.ucfirst($name).'.php';
-
-            if($this->fileSystem->put($this->file,$content)!==FALSE){
-                $this->name = $name;
-                $this->replaceFileContent($replacement,$this->file);
-            }
-        }
-
-        return $this;
+        return  (isset($this->accessibleProperties[$method])) ?
+            $this->accessibleProperties[$method]
+            : 'public';
     }
 
     /**
-     * creates method to file for generator
+     * get parameters method value for generator
      *
-     * @param array $params
+     * @param $method
+     * @return mixed|string
      */
-    public function createMethod($params=array())
+    private function getMethodParameters($method)
     {
-
+        return  (isset($this->methodParameters[$method])) ?
+            str_replace('$','\$',$this->methodParameters[$method])
+            : '';
     }
 
     /**
@@ -128,15 +315,23 @@ class Generator
      *
      * @param $replacement
      * @param $file
+     * @param bool $default
      * @return void
      */
-    private function replaceFileContent($replacement,$file)
+    private function replaceFileContent($replacement,$file,$default=false)
     {
-        $replacementVariables = $this->replacementVariables($replacement);
+        $replacementVariables = ($default) ? $replacement : $this->replacementVariables($replacement);
         $content = $this->fileSystem->get($file);
 
         foreach ($replacementVariables as $key=>$replacementVariable){
-            $search = '/__'.$key.'__/';
+
+            if($default){
+                $search = '/'.$key.'/';
+            }
+            else{
+                $search = '/__'.$key.'__/';
+            }
+
             $replace = $replacementVariable;
             $content = preg_replace($search,$replace,$content);
         }
@@ -164,9 +359,14 @@ class Generator
      *
      * @param $stubPath
      */
-    public function setStubPath($stubPath)
+    public function setStubPath($stubPath=null)
     {
-        $this->stubPath = $stubPath;
+        if(is_null($stubPath)){
+            $this->stubPath = app()->corePath().'Console/Stubs/generator';
+        }
+        else{
+            $this->stubPath = $stubPath;
+        }
     }
 
 }
