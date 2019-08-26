49677425b1f49e720d68a11cca6a358ffbdfa553
aligurbuz <galiant781@gmail.com>
2019-08-26 08:55:15 +0000

    track logger support and track service provider stub fix

---
+++ src/resta/Support/TrackLogger.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Resta\Support;
+
+use Resta\Contracts\ApplicationContracts;
+use Resta\Foundation\ApplicationProvider;
+
+class TrackLogger extends ApplicationProvider
+{
+    /**
+     * @var null|array
+     */
+    protected $output;
+
+    /**
+     * @var null|array
+     */
+    protected $arguments;
+
+    /**
+     * TrackLogger constructor.
+     * @param $output
+     * @param $arguments
+     * @param ApplicationContracts $app
+     */
+    public function __construct(ApplicationContracts $app,$output,$arguments)
+    {
+        parent::__construct($app);
+        
+        if(!$this->app->runningInConsole()){
+            exception()->runtime('Console application is missing');
+        }
+        
+        $this->output = $output;
+        $this->arguments = $arguments;
+    }
+    
+    public function handle()
+    {
+        if($this->output['meta']['success'])
+        {
+            echo ''.$this->output['trackNumber'].' - SUCCESS:';
+            echo PHP_EOL;
+            echo 'Request Success : true';
+        }
+        else{
+
+            echo ''.$this->output['trackNumber'].' - ERROR:';
+            echo PHP_EOL;
+            echo 'Error: '.$this->output['resource']['errorMessage'];
+            echo PHP_EOL;
+            echo 'Error File: '.$this->output['resource']['errorFile'];
+            echo PHP_EOL;
+            echo 'Error Line: '.$this->output['resource']['errorLine'];
+            echo PHP_EOL;
+            echo 'Error Type: '.$this->output['resource']['errorType'];
+        }
+
+        echo PHP_EOL;
+        echo 'Request Code: '.$this->output['meta']['status'];
+
+        echo PHP_EOL;
+        echo 'Client Ip: '.$this->output['clientIp'];
+
+        echo PHP_EOL;
+        echo 'Endpoint: '.$this->output['requestUrl'];
+
+        echo PHP_EOL;
+        echo 'Get Data: '.json_encode($this->output['getData']);
+
+        echo PHP_EOL;
+        echo 'Post Data: '.json_encode($this->output['postData']);
+
+        echo PHP_EOL;
+        echo 'Auth: '.$this->output['auth'];
+
+        echo PHP_EOL;
+        echo 'Time: '.date('Y-m-d H:i:s');
+
+        echo PHP_EOL;
+        echo 'Client Key: '.$this->output['clientApiTokenKey'];
+
+        echo PHP_EOL;
+        echo PHP_EOL;
+    }
+}
