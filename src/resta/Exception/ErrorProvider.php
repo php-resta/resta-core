0151674e64b54e605d3da69a756dfa09843b99f1
aligurbuz <galiant781@gmail.com>
2019-10-01 13:01:18 +0000

    error provider exception extender fix

--- src/resta/Exception/ErrorProvider.php
+++ src/resta/Exception/ErrorProvider.php
@@ -215,9 +215,9 @@ class ErrorProvider extends ApplicationProvider
         //set json app exception
         $this->app->register('routerResult',$this->result);
 
-        if($this->app->has('exceptionContainer')){
-            $exceptionContainer = $this->app->get('exceptionContainer');
-            $this->result = $exceptionContainer($this->result,$this->data['status']);
+        if($this->app->has('exceptionOutput')){
+            $exceptionOutput = $this->app->get('exceptionOutput');
+            $this->result = $exceptionOutput($this->result,$this->data['status']);
         }
 
         $restaOutHandle = null;
