f1accdf29b0f94b44d8345b1f9376c9b7976ba2d
aligurbuz <galiant781@gmail.com>
2019-10-07 14:52:21 +0000

    client fix

--- src/resta/Client/Client.php
+++ src/resta/Client/Client.php
@@ -13,6 +13,7 @@ use ReflectionException as ReflectionExceptionAlias;
  * @property $this autoObjectValidate
  * @property $this requestExcept
  * @property $this expected
+ * @property  $this groups
  */
 class Client extends ClientAbstract implements HandleContracts
 {
@@ -459,6 +460,17 @@ class Client extends ClientAbstract implements HandleContracts
         // we update the input values ​​after
         // we receive and check the saved objects.
         foreach ($clientObjects as $key=>$value){
+
+            if(property_exists($this,'groups') && is_array($this->groups) && in_array($key,$this->groups)){
+
+                $this->{$key} = $clientObjects['origin'][array_search($key,$this->groups)][$key];
+                $this->inputs[$key] = $this->{$key};
+
+                // the request update to be performed using
+                // the method name to be used with the http method.
+                $this->registerRequestInputs($key);
+                unset($this->inputs[$key]);
+            }
 
             if(!in_array($key,$this->generatorList) && isset($clientObjects['origin'][$key])){
 
