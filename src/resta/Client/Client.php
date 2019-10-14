2e45d1fcf5e3ef556a83f830600daa804ec35ce4
aligurbuz <galiant781@gmail.com>
2019-10-14 12:20:37 +0000

    client test groups capsule fix

--- src/resta/Client/Client.php
+++ src/resta/Client/Client.php
@@ -143,7 +143,8 @@ class Client extends ClientAbstract implements HandleContracts
                 $this->capsule = array_merge($this->capsule,$this->auto_capsule);
             }
 
-            if(property_exists($this,'groups') && is_array($this->groups)){
+            if(property_exists($this,'groups')
+                && property_exists($this,'capsule') && is_array($this->groups)){
                 $this->capsule = array_merge($this->capsule,$this->groups);
 
                 foreach ($this->capsule as $item) {
